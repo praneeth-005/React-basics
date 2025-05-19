a05a0d7b1174de6f707d661760b472a3606dd509
praneeth <surapureddy.praneeth@gmail.com>
2025-05-19 15:35:42 +0530

    new files

---
+++ index.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div", {id : "parent"}, 
+    [React.createElement("div", {id : "child"}, [React.createElement("h1", {id : "heading1"}, "This is heading1"), React.createElement("h2", {id : "heading2"}, "This is heading 2")]),
+React.createElement("div", {id : "child2"}, [React.createElement("h3", {id : "heading3"}, "This is heading 3"), React.createElement("h4", {id : "heading4"}, "This is heading4")])]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
