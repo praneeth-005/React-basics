ee292d8b790f4f2f3ff4c4800243934fe5e1516f
praneeth <surapureddy.praneeth@gmail.com>
2025-05-19 17:31:11 +0530

    changes updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", {id : "parent"}, 
     [React.createElement("div", {id : "child"}, [React.createElement("h1", {id : "heading1"}, "This is heading1"), React.createElement("h2", {id : "heading2"}, "This is heading 2")]),
 React.createElement("div", {id : "child2"}, [React.createElement("h3", {id : "heading3"}, "This is heading 3"), React.createElement("h4", {id : "heading4"}, "This is heading4")])]
